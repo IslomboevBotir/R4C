6f937acf2f6cf4542e352b5b741c1ec2e7e03de5
BotirIslomboev <=>
2024-12-13 16:10:28 +0500

    feat(robots) : add 1) add logic for save robots in database

--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+from django.core.exceptions import ValidationError
+
+from robots.models import Robot
+
+
+class RobotsView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            model = data.get("model")
+            version = data.get("version")
+            created = data.get("created")
+            if Robot.objects.filter(model=model, version=version).exists():
+                raise ValidationError(f"Model {model} with {version} version already exist.")
+            robot = Robot(model=model, version=version, created=created)
+            robot.save()
+
+            return JsonResponse(
+                {
+                    'status': 'ok',
+                    'data': robot.to_dict(),
+                },
+                status=201,
+            )
+        except Exception as e:
+            return JsonResponse(
+                {
+                    'status': 'error',
+                    'data': str(e)
+                },
+                status=500
+            )
